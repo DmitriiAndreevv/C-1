bdd4dc15460d8983fa228a05cab5ede8bb7aafce
dmitriy.andreev.1995@inbox.ru <dmitriy.andreev.1995@inbox.ru>
2022-08-20 00:41:52 +0300

    Second Task

---
+++ Homework2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+
+Console.Clear();
+Console.Write("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число");
+int number3 = int.Parse(Console.ReadLine());
+
+int max = number;
+int avg = number2;
+int min = number3;
+
+if (number > max)max = number;
+if (number2 > max)max = number2;
+if (number3 > max)max = number3;
+{
+    Console.WriteLine(max);
+}
+Console.WriteLine($"max{max}");
+
